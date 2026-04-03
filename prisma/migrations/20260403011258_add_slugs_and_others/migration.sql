823853c84aee66c59569efa974738d3e1a31f5e0
ibraSanusi <isanu227@gmail.com>
2026-04-03 13:25:56 +0200

    Migrate slugs columns and others to JobApplication

---
+++ prisma/migrations/20260403011258_add_slugs_and_others/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "JobApplication" ADD COLUMN     "coverSlug" TEXT,
+ADD COLUMN     "cvSlug" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "medium" TEXT,
+ADD COLUMN     "salary" INTEGER;
