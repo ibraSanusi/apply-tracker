bcc319517fe1ae7ac96d9bbd1a7297c179ef6d8a
ibraSanusi <isanu227@gmail.com>
2026-04-14 00:29:16 +0200

    Add cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ import Fastify from 'fastify'
 import userRoute from './routes/userRoute.js'
 import db from './db.js'
 import applicationRoute from './routes/applicationRoute.js'
+import cors from '@fastify/cors'
 
 export async function buildApp(opts = {}) {
     const app = Fastify(opts)
     app.decorate('db', db)
+    await app.register(cors, {
+        origin: (origin, cb) => {
+            if (!origin) {
+                cb(null, true)
+                return
+            }
+            try {
+                const hostname = new URL(origin).hostname
+                if (hostname === "localhost") {
+                    cb(null, true)
+                    return
+                }
+            } catch (err) {
+                // Fall through to not allowed
+            }
+            cb(new Error("Not allowed"), false)
+        }
+    })
     await app.register(userRoute, { prefix: '/users' })
     await app.register(applicationRoute, { prefix: '/applications' })
     return app
